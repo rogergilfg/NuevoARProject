31d64727152b6ed4173c7f81b5da410923e91370
rogergilfg <rogergil.fg@gmail.com>
2026-04-13 11:46:12 +0200

    Cositas

--- Assets/Scripts/TrackingImageController.cs
+++ Assets/Scripts/TrackingImageController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
@@ -6,41 +7,61 @@ public class TrackingImageController : MonoBehaviour
 {
     [SerializeField] private ARTrackedImageManager trackedImageManager;
     [SerializeField] private GameObject fcbPrefab;
+    [SerializeField] private ARObjects[] objetosAR;
 
     private GameObject prefabCopy;
 
     private void OnEnable()
     {
-        trackedImageManager.trackedImagesChanged += OnTrackedChanged;
+        //trackedImagemanager.trackedImagesChanged += OnTrackedChanged; // Sirve para enlazar acciones. Herramienta para ponerlo todo en común (llamas a un solo evento).
+        trackedImageManager.trackablesChanged.AddListener(OnTrackedChanged);
     }
-
     private void OnDisable()
     {
-        
+        trackedImageManager.trackablesChanged.RemoveListener(OnTrackedChanged);
     }
 
-    void OnTrackedChanged(ARTrackedImagesChangedEventArgs eventargs)
+    void OnTrackedChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventargs)
     {
-        foreach(var newImage in eventargs.added)
+        foreach (var newImage in eventargs.added) //para repasar todas las img que se han añadido.
         {
-            if(newImage.referenceImage.name == "fcb")
+            for (int i = 0; i < objetosAR.Length; i++)
             {
-                prefabCopy = Instantiate(fcbPrefab, newImage.transform.position, newImage.transform.rotation);
+                if (objetosAR[i].referenceImageName == newImage.referenceImage.name)
+                {
+                    prefabCopy = Instantiate(objetosAR[i].prefab, newImage.transform.position, newImage.transform.rotation);
+                }
             }
+
         }
 
-        foreach (var newImage in eventargs.removed)
+        foreach (var newImage in eventargs.removed) //por si la imagen no se trackea
         {
-            //Eliminar prefab
-            if (newImage.referenceImage.name == "fcb")
+            //Eliminar el prefab
+            /*if (newImage.referenceImage.name == "simpleFrame")
             {
                 Destroy(prefabCopy);
-            }
+            }*/
         }
 
         foreach (var newImage in eventargs.updated)
         {
             //Esto es cada frame que sigue detectando
+
+            for (int i = 0; i < objetosAR.Length; i++)
+            {
+                if (objetosAR[i].referenceImageName == newImage.referenceImage.name && prefabCopy == null)
+                {
+                    prefabCopy = Instantiate(objetosAR[i].prefab, newImage.transform.position, newImage.transform.rotation);
+                }
+            }
         }
     }
+}
+
+[Serializable]
+public class ARObjects
+{
+    public string referenceImageName;
+    public GameObject prefab;
 }
